fd715b593d4715341d0b4e0e02fa8772e7630dcc
YarynaMryhlotska <89984528+YarynaMryhlotska@users.noreply.github.com>
2021-10-19 00:39:51 +0300

    122

---
+++ UnitTest1_laba5_1/UnitTest1_laba5_1.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1laba51
+{
+	TEST_CLASS(UnitTest1laba51)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
